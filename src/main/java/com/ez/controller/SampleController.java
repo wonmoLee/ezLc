ca11e26e09b490eebdb5273120792e4a97a76598
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2022-06-23 16:22:21 +0900

    파라미터 직접 변환 예제

--- src/main/java/com/ez/controller/SampleController.java
+++ src/main/java/com/ez/controller/SampleController.java
@@ -1,14 +1,19 @@
 package com.ez.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ez.domain.SampleDTO;
 import com.ez.domain.SampleDTOList;
+import com.ez.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,6 +21,13 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+	// 파라미터를 직접 변환해서 사용해야할 때 사용
+//	@InitBinder
+//	public void InitBinder(WebDataBinder binder) {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+//		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+//	}
+	
 	@GetMapping("/ex01")
 	public String ex01(SampleDTO dto) {
 		log.info("" + dto);
@@ -43,5 +55,12 @@ public class SampleController {
 		log.info("list dtos: " + list);
 		
 		return "ex02Bean";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo);
+		
+		return "ex03";
 	}
 }
