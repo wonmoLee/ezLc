245cba0ad692c4839ac5733b01ad5f12cbfd5e3f
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2022-06-23 13:09:50 +0900

    sql 파라메타확인을 위한 log설정

--- src/main/java/com/ez/config/RootConfig.java
+++ src/main/java/com/ez/config/RootConfig.java
@@ -23,8 +23,8 @@ public class RootConfig {
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@61.75.138.220:1521:XE");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@61.75.138.220:1521:XE");
 		hikariConfig.setUsername("ezLc");
 		hikariConfig.setPassword("password를 입력하세요.");
 		
