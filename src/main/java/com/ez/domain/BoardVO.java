a89f95745adb2624e925b65ffde1c94dad47fba3
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2022-06-30 13:13:11 +0900

    게시글 관련 일자 데이터 포맷 변경

--- src/main/java/com/ez/domain/BoardVO.java
+++ src/main/java/com/ez/domain/BoardVO.java
@@ -1,7 +1,5 @@
 package com.ez.domain;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
@@ -10,9 +8,9 @@ public class BoardVO {
 	private String TITLE;
 	private String CONTENT;
 	private String REG_ID;
-	private Date REG_DT;
+	private String REG_DT;
 	private String REG_IP;
 	private String MOD_ID;
-	private Date MOD_DT;
+	private String MOD_DT;
 	private String MOD_IP;
 }
